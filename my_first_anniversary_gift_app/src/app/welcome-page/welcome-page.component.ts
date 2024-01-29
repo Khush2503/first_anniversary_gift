65aa7a5b3bfc99589c3e497a5381efaed8ed8a03
Khush2503 <khushboo143.jsr@gmail.com>
2024-01-30 01:19:24 +0530

    created login form

--- my_first_anniversary_gift_app/src/app/welcome-page/welcome-page.component.ts
+++ my_first_anniversary_gift_app/src/app/welcome-page/welcome-page.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [NgbModule],
+  imports: [NgbModule, ReactiveFormsModule, FormsModule],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.scss'
 })
-export class WelcomePageComponent {
+export class WelcomePageComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    user: new FormControl(''),
+    password: new FormControl(''),
+    acceptTerms: new FormControl(false),
+  });
+
+  submitted = false;
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit() : void {
+    this.loginForm = this.fb.group(
+      {
+        user: ['', Validators.required],
+        password: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue]
+      },
+    );
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.loginForm.value, null, 2));
+  }
 
 }
