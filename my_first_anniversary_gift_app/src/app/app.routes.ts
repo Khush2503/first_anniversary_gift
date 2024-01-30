8fc6e4d6198bda93e03df99888108954a3b520d9
Khush2503 <khushboo143.jsr@gmail.com>
2024-01-30 20:56:49 +0530

    created gift page

--- my_first_anniversary_gift_app/src/app/app.routes.ts
+++ my_first_anniversary_gift_app/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { GiftPageComponent } from './gift-page/gift-page.component';
 
 export const routes: Routes = [
-    { path: '', component: WelcomePageComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+    { path: 'welcome', component: WelcomePageComponent },
+    { path: 'surprise', component: GiftPageComponent},
 ];
--- my_first_anniversary_gift_app/src/app/welcome-page/welcome-page.component.ts
+++ my_first_anniversary_gift_app/src/app/welcome-page/welcome-page.component.ts
@@ -1,13 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [NgbModule, ReactiveFormsModule, FormsModule],
+  imports: [NgbModule, ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './welcome-page.component.html',
-  styleUrl: './welcome-page.component.scss'
+  styleUrl: './welcome-page.component.scss',
 })
 export class WelcomePageComponent implements OnInit {
   loginForm: FormGroup = new FormGroup({
@@ -18,26 +28,38 @@ export class WelcomePageComponent implements OnInit {
 
   submitted = false;
 
-  constructor(private readonly fb: FormBuilder) {}
+  formText = 'Avi.Khush@2023';
 
-  ngOnInit() : void {
-    this.loginForm = this.fb.group(
-      {
-        user: ['', Validators.required],
-        password: ['', Validators.required],
-        acceptTerms: [false, Validators.requiredTrue]
-      },
-    );
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      user: ['', Validators.required],
+      password: ['', Validators.required],
+      acceptTerms: [false, Validators.requiredTrue],
+    });
+  }
+
+  get controls(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
   }
 
   onSubmit(): void {
     this.submitted = true;
 
+    console.log(this.loginForm);
+
     if (this.loginForm.invalid) {
       return;
     }
-
-    console.log(JSON.stringify(this.loginForm.value, null, 2));
+    if(this.controls['user'].value == this.formText && this.controls['password'].value == this.formText) {
+      window.localStorage.setItem("isAuthenticated", "true");
+      this.router.navigate(['/surprise']);
+    } else{
+      window.alert("Please enter correct details!");
+    }
   }
-
 }
